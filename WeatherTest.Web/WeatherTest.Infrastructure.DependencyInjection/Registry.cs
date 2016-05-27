72ca07f1584ea1312a6de088b8b5b244ca687cca
jinwood-4com <julianinwood@gmail.com>
2016-05-27 20:49:34 +0100

    Scaffold DI

---
+++ WeatherTest.Web/WeatherTest.Infrastructure.DependencyInjection/Registry.cs
@@ -0,0 +1,14 @@
+﻿using SimpleInjector.Packaging;
+using System;
+using SimpleInjector;
+
+namespace WeatherTest.Infrastructure.DependencyInjection
+{
+    public class Registry : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
